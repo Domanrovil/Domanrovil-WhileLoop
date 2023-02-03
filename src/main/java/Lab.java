a22a664731509b218703d362d92f6e097775b7f4
Domanrovil Ovalle <domanrovil.ovalle@gmail.com>
2023-02-03 03:57:05 +0000

    Completed Lab While Loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -35,7 +35,10 @@ public class Lab {
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
-
+        while (i <end){
+            sum = sum + i;
+            i = i +1;
+        }
 //        code here
 
         return sum;
